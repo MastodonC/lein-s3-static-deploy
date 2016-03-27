7c96f4337ba5e71efe289e884a1fa9d8c3f4bc58
Axel Schüssler <axel.schuessler@gmail.com>
2016-03-27 12:36:50 +0200

    add explicit dependency on data.json

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-aws-s3 "0.3.7"]
                  [me.kanej/s3-sync "0.4.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/java.data "0.1.1"]]
   :eval-in-leiningen true
   :min-lein-version "2.0.0"
